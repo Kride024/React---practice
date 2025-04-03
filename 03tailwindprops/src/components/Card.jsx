0f48bba795d02c0bc65f82039a59b7c2582eb8b8
Kride024 <kritiyadavcoding@gmail.com>
2025-04-03 22:46:09 +0530

    Commit

--- 03tailwindprops/src/components/Card.jsx
+++ 03tailwindprops/src/components/Card.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Card(props) {
+function Card({username,Num="4"}) {
  
 
   
@@ -11,10 +11,10 @@ function Card(props) {
     <img className="size-48 shadow-xl rounded-md" alt="" src="https://tailwindcss.com/_next/static/media/cover.de1997f7.png" />
   </div>
   <div className="flex items-center md:items-start  flex-col grid-rows-3">
-    <span className="text-2xl font-medium">{props.username}</span>
+    <span className="text-2xl font-medium">{username}</span>
     <span className="font-medium text-sky-500">The Anti-Patterns</span>
     <span className="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
-      <span>No. {props.Num || 4}</span>
+      <span>No. {Num}</span>
       <span>·</span>
       <span>2025</span>
     </span>
