8cf45f2e77b2731369d822c55b004763a60e3e62
Kride024 <kritiyadavcoding@gmail.com>
2025-04-03 22:41:24 +0530

    commit

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -5,13 +5,15 @@ import './App.css'
 import Card from './components/Card'
 
 function App() {
+ 
+  let arr=[1,2,3,4,5]
   const [count, setCount] = useState(0)
 
   return (
     <>
     <h1 className='bg-green-400 text-black p-4 rounded-xl mb-5'>Tailwind Test</h1>
-   <Card/>
-   <Card/>
+   <Card username="chai aur code" Num="4" />
+   <Card username={arr} Num="5"/>
   </>
   )
 }
--- 03tailwindprops/src/components/Card.jsx
+++ 03tailwindprops/src/components/Card.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 
 function Card(props) {
-  console.log("props",props);
+ 
+
   
   return (
     <div>
-   <div class="flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl">
+   <div className="flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl">
   <div>
-    <img class="size-48 shadow-xl rounded-md" alt="" src="https://tailwindcss.com/_next/static/media/cover.de1997f7.png" />
+    <img className="size-48 shadow-xl rounded-md" alt="" src="https://tailwindcss.com/_next/static/media/cover.de1997f7.png" />
   </div>
-  <div class="flex items-center md:items-start  flex-col grid-rows-3">
-    <span class="text-2xl font-medium">Class Warfare</span>
-    <span class="font-medium text-sky-500">The Anti-Patterns</span>
-    <span class="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
-      <span>No. 4</span>
+  <div className="flex items-center md:items-start  flex-col grid-rows-3">
+    <span className="text-2xl font-medium">{props.username}</span>
+    <span className="font-medium text-sky-500">The Anti-Patterns</span>
+    <span className="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
+      <span>No. {props.Num || 4}</span>
       <span>·</span>
       <span>2025</span>
     </span>
