276cbbebaa8147c0f05ef956c624f1c374920b27
Kride024 <kritiyadavcoding@gmail.com>
2025-06-08 07:23:53 +0530

    Commit

--- 06currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -5,12 +5,12 @@ const [data,setData]=useState({});
 useEffect(()=>{
     fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`)
     .then((res)=>res.json())
-    .then((res)=>setData[res[currency]]);
+    .then((res)=>setData(res[currency] || {}));
     console.log(data);
     
 },[currency]);
 //return setData,[data,setData] all can'nt return in this state.
-return data; // data is object
+return data ;// data is object
 }
 
 export default useCurrencyInfo;//we are returning function directly and from there we can take excess to currency from data.
